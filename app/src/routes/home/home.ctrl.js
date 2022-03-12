0fdea26ad65256e2f6143db766744e5d139f3236
seowon <jessica7654@naver.com>
2022-03-13 06:41:06 +0900

    회원가입 페이지 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -6,10 +6,12 @@ const output = {
   home : (req, res) => {
     res.render("home/index");
   },
-  
   login : (req, res) => {
     res.render("home/login");
   },
+  register : (req, res) => {
+    res.render("home/register");
+  }
 };
 // render - 해당 경로로 왔을 때 넘겨줄 html페이지를 지정해 줌
 
